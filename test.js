a8e098de143dc1715d629312584d685ef836ead8
Zacharie Day <day@gwu.edu>
2019-05-16 19:13:10 -0400

    Tidy up testing suite

--- test.js
+++ test.js
@@ -1,28 +1,84 @@
 import { autofurigana } from './autofurigana';
 
-// Test basic, one-kanji conversion.
-console.log(autofurigana('家', 'いえ'));
+Array.prototype.equals = function(array) {
+  if (!array)
+    return false;
 
-// Multi-kanji conversion.
-console.log(autofurigana('昨日', 'きのう'));
+  if (this.length != array.length)
+    return false;
 
-// Kanji with trailing kana.
-console.log(autofurigana('走る', 'はしる'));
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      if (!this[i].equals(array[i]))
+        return false;       
+    } else if (this[i] != array[i]) {
+      return false;
+    }
+  }
 
-// Long string with mixture of kana and kanji.
-console.log(autofurigana(
+  return true;
+}
+
+const test = (desc, kanji, kana, out) => {
+  if (typeof test.ct_pass == 'undefined')
+    test.ct_pass = 0;
+  if (typeof test.ct_fail == 'undefined')
+    test.ct_fail = 0;
+
+  process.stdout.write("Test: " + desc + "... ");
+  let res = autofurigana(kanji, kana);
+  if (res.equals(out)) {
+    process.stdout.write("PASS\n");
+    test.ct_pass++;
+  } else {
+    process.stdout.write("FAIL\n");
+    console.log(res);
+    console.log(out);
+    test.ct_fail++;
+  }
+}
+
+test(
+  'Single kanji',
+  '家',
+  'いえ',
+  [ [ '家', 'いえ' ] ]
+);
+
+test(
+  'Multiple kanji',
+  '昨日',
+  'きのう',
+  [ [ '昨日', 'きのう' ] ]
+);
+
+test(
+  'Kanji with kana',
+  '走る',
+  'はしる',
+  [ [ '走', 'はし' ], [ 'る', null ] ]
+);
+
+test(
+  'Long mixture',
   '走ることについて語るときに僕の語ること',
-  'はしることについてかたるときにぼくのかたること'
-));
+  'はしることについてかたるときにぼくのかたること',
+  [ [ '走', 'はし' ],
+    [ 'ることについて', null ],
+    [ '語', 'かた' ],
+    [ 'るときに', null ],
+    [ '僕', 'ぼく' ],
+    [ 'の', null ],
+    [ '語', 'かた' ],
+    [ 'ること', null ] ]
+);
 
-// String that could confuse the function.
-console.log(autofurigana(
+test(
+  'String that could confuse the function',
   '息抜き',
-  'いきぬき'
-));
+  'いきぬき',
+  [ [ '息抜', 'いきぬ' ], [ 'き', null ] ]
+);
 
-// Invalid input.
-console.log(autofurigana(
-  '一直線に',
-  'いっちょくせん'
-));
+console.log(test.ct_pass + ' tests passed.');
+console.log(test.ct_fail + ' tests failed.');
